fbbb5e5a5533448a63110e4a54bdcf3992ffa85d
rolindaz <rolinda.zamboni@gmail.com>
2026-01-13 13:19:40 +0100

    feat: add migration files for yarn, fiber, and project_yarn translations with relationships

---
+++ database/migrations/2026_01_13_121643_create_project_yarn_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_yarn', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('projects');
+            $table->foreignId('yarn_id')->constrained('yarns');
+            $table->decimal('quantity');
+            $table->integer('meterage');
+            $table->integer('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_yarn');
+    }
+};
