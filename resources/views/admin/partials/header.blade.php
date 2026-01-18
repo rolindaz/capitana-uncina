60bad8baa3a55d33006aca57fc909fc0ee5e6ba7
rolindaz <rolinda.zamboni@gmail.com>
2026-01-18 18:45:51 +0100

    feat: update header layout and styles; enhance logout button design and add font styling

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -5,19 +5,16 @@
     <x-search-bar/>
     
     <img class="geko" src="{{ asset('storage/element2.png') }}" alt="geko">
-    
 
     <div class="d-flex align-items-center gap-3">
-        <span class="small text-muted">
+        <span class="small" style="font-family: 'Walter Turncoat'; font-weight: 600;">
             {{ auth()->user()->name ?? 'Admin' }}
         </span>
-
-        <img
-            src="https://ui-avatars.com/api/?name=Admin"
-            class="rounded-circle"
-            width="32"
-            height="32"
-        >
+        -
+        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-sm" style="font-family: 'Walter Turncoat'; font-weight: 600;">Logout</button>
+        </form>
     </div>
 
     <img class="chameleon" src="{{ asset('storage/element3.png') }}" alt="chameleon">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Permanent+Marker&family=Walter+Turncoat&display=swap" rel="stylesheet">
 
     <!-- Usando Vite -->
@@ -62,7 +63,7 @@
         </main>
 
 
-        
+
             <!-- Login Modal -->
             <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
