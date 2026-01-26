c9a3ed16e5aa6defab39ee262c032243ea1da79d
rolindaz <rolinda.zamboni@gmail.com>
2026-01-26 17:32:03 +0100

    feat: update colorway keys for consistency and add Italian translations for colorways

--- config/data/colorways.php
+++ config/data/colorways.php
@@ -10,67 +10,67 @@ return [
     [
         'id' => 2,
         'color_code' => 'uni colour 58',
-        'key' => 'ametista',
+        'key' => 'amethyst',
         'image_path' => 'colorways/ametista.png'        
     ],
     [
         'id' => 3,
         'color_code' => 'uni colour 05',
-        'key' => 'lavanda',
+        'key' => 'lavender',
         'image_path' => 'colorways/lavanda.png'        
     ],
     [
         'id' => 4,
         'color_code' => 'uni colour 62',
-        'key' => 'limone_safran',
+        'key' => 'lemon_safran',
         'image_path' => 'colorways/limone.png'        
     ],
     [
         'id' => 5,
         'color_code' => 'uni colour 60',
-        'key' => 'verde_muschio',
+        'key' => 'moss_green',
         'image_path' => 'colorways/verde-muschio.png'        
     ],
     [
         'id' => 6,
         'color_code' => 'uni colour 63',
-        'key' => 'verde_mare_safran',
+        'key' => 'sea_green_safran',
         'image_path' => 'colorways/verde-mare.png'        
     ],
     [
         'id' => 7,
         'color_code' => 'uni colour 61',
-        'key' => 'te_verde',
+        'key' => 'green_tea',
         'image_path' => 'colorways/te-verde.png'
     ],
     [
         'id' => 8,
         'color_code' => 'uni colour 31',
-        'key' => 'pistacchio_safran',
+        'key' => 'apple_green',
         'image_path' => 'colorways/pistacchio.png'        
     ],
     [
         'id' => 9,
         'color_code' => 'uni colour 20',
-        'key' => 'ciliegia',
+        'key' => 'cherry',
         'image_path' => 'colorways/ciliegia.png'        
     ],
     [
         'id' => 10,
         'color_code' => 'uni colour 16',
-        'key' => 'nero_safran',
+        'key' => 'black_safran',
         'image_path' => 'colorways/nero.png'        
     ],
     [
         'id' => 11,
         'color_code' => 'uni colour 17',
-        'key' => 'bianco_safran',
+        'key' => 'white_safran',
         'image_path' => 'colorways/bianco.png'        
     ],
     [
         'id' => 12,
         'color_code' => 'uni colour 15',
-        'key' => 'fucsia',
+        'key' => 'plum',
         'image_path' => 'colorways/fucsia.png'        
     ],
     [
@@ -88,26 +88,26 @@ return [
     [
         'id' => 15,
         'color_code' => '08210',
-        'key' => 'giallo',
-        'image_path' => 'colorways/giallo.png'        
+        'key' => 'yellow_bravo',
+        'image_path' => 'colorways/giallo-bravo.png'        
     ],
     [
         'id' => 16,
         'color_code' => '08337',
-        'key' => 'gold',
-        'image_path' => 'colorways/gold.png'        
+        'key' => 'gold_bravo',
+        'image_path' => 'colorways/gold-bravo.png'        
     ],
     [
         'id' => 17,
         'color_code' => '08351',
-        'key' => 'kiwi',
-        'image_path' => 'colorways/kiwi.png'        
+        'key' => 'kiwi_green',
+        'image_path' => 'colorways/kiwi-green.png'        
     ],
     [
         'id' => 18,
         'color_code' => '08365',
-        'key' => 'lilac',
-        'image_path' => 'colorways/lilac.png'        
+        'key' => 'lilac_bravo',
+        'image_path' => 'colorways/lilac-bravo.png'        
     ],
     [
         'id' => 19,
@@ -118,13 +118,13 @@ return [
     [
         'id' => 20,
         'color_code' => 'uni colour 69',
-        'key' => 'pistacchio_paris',
+        'key' => 'pistachio_paris',
         'image_path' => 'colorways/pistacchio2.png'        
     ],
     [
         'id' => 21,
         'color_code' => 'uni colour 108',
-        'key' => 'corallo_loves_you_9',
+        'key' => 'coral_loves_you_9',
         'image_path' => 'colorways/corallo.png'        
     ],
     [
@@ -136,49 +136,49 @@ return [
     [
         'id' => 23,
         'color_code' => '08224',
-        'key' => 'bianco_bravo',
+        'key' => 'white_bravo',
         'image_path' => 'colorways/bianco2.png'        
     ],
     [
         'id' => 24,
         'color_code' => '08378',
-        'key' => 'verde_mare_bravo',
+        'key' => 'sea_green_bravo',
         'image_path' => 'colorways/verde-mare2.png'        
     ],
     [
         'id' => 25,
         'color_code' => '08307',
-        'key' => 'dalia',
+        'key' => 'dahlia',
         'image_path' => 'colorways/dalia.png'        
     ],
     [
         'id' => 26,
         'color_code' => '08361',
-        'key' => 'lemon',
+        'key' => 'lemon_bravo',
         'image_path' => 'colorways/limone2.png'        
     ],
     [
         'id' => 27,
         'color_code' => '08340',
-        'key' => 'cobalto',
+        'key' => 'cobalt',
         'image_path' => 'colorways/cobalto.png'        
     ],
     [
         'id' => 28,
         'color_code' => '08194',
-        'key' => 'limone',
+        'key' => 'lime',
         'image_path' => 'colorways/limone3.png'        
     ],
     [
         'id' => 29,
         'color_code' => '08363',
-        'key' => 'ghiaccio',
+        'key' => 'glacier',
         'image_path' => 'colorways/ghiaccio.png'        
     ],
     [
         'id' => 30,
         'color_code' => '388',
-        'key' => 'ruggine_catona',
+        'key' => 'rust_catona',
         'image_path' => 'colorways/ruggine.png'        
     ],
     [
@@ -190,19 +190,19 @@ return [
     [
         'id' => 32,
         'color_code' => '504',
-        'key' => 'mattone',
+        'key' => 'brick_red',
         'image_path' => 'colorways/mattone.png'        
     ],
     [
         'id' => 33,
         'color_code' => '524',
-        'key' => 'albicocca',
+        'key' => 'apricot',
         'image_path' => 'colorways/albicocca.png'        
     ],
     [
         'id' => 34,
         'color_code' => '391',
-        'key' => 'oceano_profondo',
+        'key' => 'deep_ocean',
         'image_path' => 'colorways/oceano-profondo.png'        
     ],
     [
@@ -214,19 +214,19 @@ return [
     [
         'id' => 36,
         'color_code' => '395',
-        'key' => 'salice',
+        'key' => 'willow',
         'image_path' => 'colorways/salice.png'        
     ],
     [
         'id' => 37,
         'color_code' => '525',
-        'key' => 'abete',
+        'key' => 'fir',
         'image_path' => 'colorways/abete.png'        
     ],
     [
         'id' => 38,
         'color_code' => '027',
-        'key' => 'autunno',
+        'key' => 'autumn',
         'image_path' => 'colorways/autunno.png'        
     ],
     [
@@ -239,31 +239,31 @@ return [
     [
         'id' => 40,
         'color_code' => 'uni colour 08',
-        'key' => 'viola_scuro',
+        'key' => 'dark_purple',
         'image_path' => 'colorways/viola-scuro.png'        
     ],
     [
         'id' => 41,
         'color_code' => 'uni colour 65',
-        'key' => 'ruggine_paris',
+        'key' => 'rust_paris',
         'image_path' => 'colorways/ruggine2.png'        
     ],
     [
         'id' => 42,
         'color_code' => 'uni colour 41',
-        'key' => 'senape_paris',
+        'key' => 'mustard_paris',
         'image_path' => 'colorways/senape2.png'        
     ],
     [
         'id' => 43,
         'color_code' => '1371',
-        'key' => 'glicine',
+        'key' => 'wisteria',
         'image_path' => 'colorways/glicine.png'        
     ],
     [
         'id' => 44,
         'color_code' => 'uni colour 18',
-        'key' => 'senape_melody',
+        'key' => 'mustard_melody',
         'image_path' => 'colorways/senape-melody.png'        
     ],
     [
@@ -275,121 +275,121 @@ return [
     [
         'id' => 46,
         'color_code' => 'uni colour 19',
-        'key' => 'rosso_safran',
+        'key' => 'red_safran',
         'image_path' => 'colorways/rosso-safran.png'        
     ],
     [
         'id' => 47,
         'color_code' => 'uni colour 21',
-        'key' => 'arachide',
+        'key' => 'peanut',
         'image_path' => 'colorways/arachide.png'        
     ],
     [
         'id' => 48,
         'color_code' => 'uni colour 01',
-        'key' => 'rosa_chiaro',
+        'key' => 'desert_rose',
         'image_path' => 'colorways/rosa-chiaro.png'        
     ],
     [
         'id' => 49,
         'color_code' => 'uni colour 70',
-        'key' => 'orchidea_dolce',
+        'key' => 'sweet_orchid',
         'image_path' => 'colorways/orchidea-dolce.png'        
     ],
     [
         'id' => 50,
         'color_code' => 'uni colour 07',
-        'key' => 'grigio_medio',
+        'key' => 'grey',
         'image_path' => 'colorways/grigio-medio.png'        
     ],
     [
         'id' => 51,
         'color_code' => 'uni colour 69',
-        'key' => 'fard',
+        'key' => 'blush',
         'image_path' => 'colorways/fard.png'
     ],
     [
         'id' => 52,
         'color_code' => 'uni colour 13',
-        'key' => 'corallo_safran',
+        'key' => 'coral_safran',
         'image_path' => 'colorways/corallo-safran.png'        
     ],
     [
         'id' => 53,
         'color_code' => 'uni colour 09',
-        'key' => 'blu_marina',
+        'key' => 'navy_blue_safran',
         'image_path' => 'colorways/blu-marina.png'        
     ],
     [
         'id' => 54,
         'color_code' => 'uni colour 56',
-        'key' => 'rosa_cipria',
+        'key' => 'powder_pink',
         'image_path' => 'colorways/rosa-cipria.png'        
     ],
     [
         'id' => 55,
         'color_code' => 'uni colour 30',
-        'key' => 'turchese',
+        'key' => 'turquoise',
         'image_path' => 'colorways/turchese.png'        
     ],
     [
         'id' => 56,
         'color_code' => 'uni colour 67',
-        'key' => 'zucca',
+        'key' => 'pumpkin',
         'image_path' => 'colorways/zucca.png'        
     ],
     [
         'id' => 57,
         'color_code' => 'uni colour 11',
-        'key' => 'sole',
+        'key' => 'sunshine',
         'image_path' => 'colorways/sole.png'        
     ],
     [
         'id' => 58,
         'color_code' => 'uni colour 59',
-        'key' => 'argilla-rossa',
+        'key' => 'red-clay',
         'image_path' => 'colorways/argilla-rossa.png'        
     ],
     [
         'id' => 59,
         'color_code' => 'uni colour 06',
-        'key' => 'blu_denim',
+        'key' => 'denim_blue',
         'image_path' => 'colorways/blu-denim.png'        
     ],
     [
         'id' => 60,
         'color_code' => 'uni colour 50',
-        'key' => 'menta',
+        'key' => 'mint',
         'image_path' => 'colorways/menta.png'        
     ],
     [
         'id' => 61,
         'color_code' => 'uni colour 18',
-        'key' => 'panna',
+        'key' => 'off_white',
         'image_path' => 'colorways/panna.png'        
     ],
     [
         'id' => 62,
         'color_code' => '004',
-        'key' => 'rosa_freddo',
+        'key' => 'cold_pink',
         'image_path' => 'colorways/rosa-freddo.jpeg',
     ],
     [
         'id' => 63,
         'color_code' => 'uni colour 14',
-        'key' => 'dente_di_leone',
+        'key' => 'dandelion',
         'image_path' => 'colorways/dente-di-leone.png'        
     ],
     [
         'id' => 64,
         'color_code' => 'uni colour 28',
-        'key' => 'blu_marina_paris',
+        'key' => 'navy_blue_paris',
         'image_path' => 'colorways/blu-marina-paris.png'        
     ],
     [
         'id' => 65,
         'color_code' => '535',
-        'key' => 'rosso_spendobenemini',
+        'key' => 'red_spendobenemini',
         'image_path' => 'colorways/rosso-spendobenemini.png'        
     ],
     [
@@ -413,7 +413,7 @@ return [
     [
         'id' => 69,
         'color_code' => 'uni colour 08',
-        'key' => 'petrolio',
+        'key' => 'petrol',
         'image_path' => 'colorways/petrolio.png'        
     ],
 ];
