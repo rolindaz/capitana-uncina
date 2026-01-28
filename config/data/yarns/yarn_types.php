e2696551be5b3751d74cdbf1206bccee1606ab6b
rolindaz <rolinda.zamboni@gmail.com>
2026-01-28 20:31:21 +0100

    feat: Remove old yarn categories and add comprehensive yarn types data

---
+++ config/data/yarns/yarn_types.php
@@ -0,0 +1,137 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'yarn_standard_id' => 1,
+        'base_key' => 'fingering',
+        'key' => 'fingering_lace',
+        'name' => 'Fingering (Lace)'
+    ],
+    [
+        'id' => 2,
+        'yarn_standard_id' => 1,
+        'base_key' => 'thread',
+        'key' => 'thread',
+        'name' => 'Thread'
+    ],
+    [
+        'id' => 3,
+        'yarn_standard_id' => 2,
+        'base_key' => 'fingering',
+        'key' => 'fingering_super_fine',
+        'name' => 'Fingering (Super Fine)'
+    ],
+    [
+        'id' => 4,
+        'yarn_standard_id' => 2,
+        'base_key' => 'sock',
+        'key' => 'sock',
+        'name' => 'Sock'
+    ],
+    [
+        'id' => 5,
+        'yarn_standard_id' => 2,
+        'base_key' => 'baby',
+        'key' => 'baby_super_fine',
+        'name' => 'Baby (Super Fine)'
+    ],
+    [
+        'id' => 6,
+        'yarn_standard_id' => 3,
+        'base_key' => 'baby',
+        'key' => 'baby_fine',
+        'name' => 'Baby (Fine)'
+    ],
+    [
+        'id' => 7,
+        'yarn_standard_id' => 3,
+        'base_key' => 'sport',
+        'key' => 'sport',
+        'name' => 'Sport'
+    ],
+    [
+        'id' => 8,
+        'yarn_standard_id' => 4,
+        'base_key' => 'dk',
+        'key' => 'dk',
+        'name' => 'DK'
+    ],
+    [
+        'id' => 9,
+        'yarn_standard_id' => 4,
+        'base_key' => 'light_worsted',
+        'key' => 'light_worsted',
+        'name' => 'Light Worsted'
+    ],
+    [
+        'id' => 10,
+        'yarn_standard_id' => 5,
+        'base_key' => 'worsted',
+        'key' => 'worsted',
+        'name' => 'Worsted'
+    ],
+    [
+        'id' => 11,
+        'yarn_standard_id' => 5,
+        'base_key' => 'afghan',
+        'key' => 'afghan',
+        'name' => 'Afghan'
+    ],
+    [
+        'id' => 12,
+        'yarn_standard_id' => 5,
+        'base_key' => 'aran',
+        'key' => 'aran',
+        'name' => 'Aran'
+    ],
+    [
+        'id' => 13,
+        'yarn_standard_id' => 6,
+        'base_key' => 'chunky',
+        'key' => 'chunky',
+        'name' => 'Chunky'
+    ],
+    [
+        'id' => 14,
+        'yarn_standard_id' => 6,
+        'base_key' => 'craft',
+        'key' => 'craft',
+        'name' => 'Craft'
+    ],
+    [
+        'id' => 15,
+        'yarn_standard_id' => 6,
+        'base_key' => 'rug',
+        'key' => 'rug',
+        'name' => 'Rug'
+    ],
+    [
+        'id' => 16,
+        'yarn_standard_id' => 7,
+        'base_key' => 'super_bulky',
+        'key' => 'super_bulky',
+        'name' => 'Super Bulky'
+    ],
+    [
+        'id' => 17,
+        'yarn_standard_id' => 7,
+        'base_key' => 'roving',
+        'key' => 'roving_super_bulky',
+        'name' => 'Roving (Super Bulky)'
+    ],
+    [
+        'id' => 18,
+        'yarn_standard_id' => 8,
+        'base_key' => 'roving',
+        'key' => 'roving_jumbo',
+        'name' => 'Roving (Jumbo)'
+    ],
+    [
+        'id' => 19,
+        'yarn_standard_id' => 8,
+        'base_key' => 'jumbo',
+        'key' => 'jumbo',
+        'name' => 'Jumbo'
+    ],
+];
